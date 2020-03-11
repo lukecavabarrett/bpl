88b98135a70a0347f55b5e4a43f53af7fde7c8aa
lukecavabarrett <luca.cavallery@gmail.com>
2020-03-11 10:42:41 +0000

    cosmetic fixes

--- apps/sample.cc
+++ apps/sample.cc
@@ -25,7 +25,22 @@ void eqtest(){
   //unifying (X,X) with (X,a) doesn't produce the desired result
 }
 
+void occurscheck_test(){
+  db storage;
+  storage.insert_clause(parser::parse_clause("eq(X,X)."));
+  //bugs:
+  query::execute_query(storage,parser::parse_term_list("eq(X,f(X))."));
+
+}
+
+void print_test(){
+  db storage;
+  storage.insert_clause(parser::parse_clause("eq(A,A)."));
+  //bugs:
+  query::execute_query(storage,parser::parse_term_list("eq(X,X)."));
+}
+
 
 int main(){
-  geqtest();
+print_test();
 }
--- libs/query/query.cc
+++ libs/query/query.cc
@@ -22,8 +22,8 @@ uint32_t make_insert_expr_id(std::unordered_map<std::string, uint32_t> &vartable
   //function
   std::vector<uint32_t> children;
   for (const parser::expression &sexpr : expr.args)children.push_back(make_insert_expr_id(vartable, sexpr, storage, env));
-  uint32_t fid = storage.function_id(expr.name,expr.args.size());
-  assert(children.size()==storage.functions[fid].degree);
+  uint32_t fid = storage.function_id(expr.name, expr.args.size());
+  assert(children.size() == storage.functions[fid].degree);
   uint32_t id = env.new_element_function(fid, std::move(children));
   return id;
 }
@@ -34,63 +34,90 @@ struct term {
   uint32_t predicate_id;
   std::vector<uint32_t> args; //reference ufds world
   term() = default;
-  term(const db::predicate::clause::rhs_item& it,uint32_t delta) : predicate_id(it.predicate_id) {
+  term(const db::predicate::clause::rhs_item &it, uint32_t delta) : predicate_id(it.predicate_id) {
     args.reserve(it.args.size());
-    for(const auto&x:it.args)args.push_back(delta+x);
+    for (const auto &x:it.args)args.push_back(delta + x);
   }
 };
 
-void print_follow(int i,int lt,const bpl::db &storage,ds::environment& env, const std::vector<std::string>& vars){
-  const ds::environment::node_t& n = env.nodes[env.get_boss_id(i)];
+bool print_follow(bool dirty, int initial, int i, int lt, const bpl::db &storage, ds::environment &env, const std::vector<std::string> &vars, std::string_view preamble) {
+  const ds::environment::node_t &n = env.nodes[env.get_boss_id(i)];
   uint32_t min_id = n.topology & 0x7ffffffu;
-  if(n.function_id==0 || min_id<lt){
+  if (n.function_id == 0 || min_id < lt) {
+    if (!dirty && min_id == initial)return false;
     //pure or already described
-    if(min_id<vars.size())std::cout<<vars[min_id];
-    else std::cout<<"_"<<min_id;
-    return;
+    if (!dirty)std::cout << preamble << vars[i] << " = ";
+    if (min_id < vars.size())std::cout << vars[min_id];
+    else std::cout << "_" << min_id;
+    return true;
   }
-  //if(i==lt)--lt; //decrease lt from second iteration onwards
+  if (initial == lt)++lt; //increase lt from second iteration onwards
+  if (!dirty)std::cout << preamble << vars[i] << " = ";
   std::cout << storage.functions[n.function_id].name;
-  if(n.args_id.empty())return;
+  if (n.args_id.empty())return true;
   std::cout << '(';
-  for(int j = 0;j<n.args_id.size();++j){
-    print_follow(n.args_id[j],lt,storage,env,vars);
-    std::cout << (j==(n.args_id.size()-1) ? ')' : ',');
+  for (int j = 0; j < n.args_id.size(); ++j) {
+    print_follow(true, initial, n.args_id[j], lt, storage, env, vars, preamble);
+    std::cout << (j == (n.args_id.size() - 1) ? ')' : ',');
   }
+  return true;
+
 }
 
-void describe_solution(const bpl::db &storage,ds::environment& env, const std::vector<std::string>& vars){
+void describe_solution(const bpl::db &storage, ds::environment &env, const std::vector<std::string> &vars) {
   bool anypr = false;
-  for(uint32_t i = 0; i < vars.size();i++){
-    std::cout << vars[i] << " = ";
-    print_follow(i,i,storage,env,vars);
-    std::cout << std::endl;
-    anypr=true;
+  std::string preamble = "";
+  for (uint32_t i = 0; i < vars.size(); i++) {
+    if (print_follow(false, i, i, i, storage, env, vars, preamble)) {
+      preamble = ",\n";
+      anypr = true;
+    }
   }
-  if(!anypr)std::cout << "true."<<std::endl;
+  if (anypr)std::cout << "." << std::endl;
+  if (!anypr)std::cout << "true." << std::endl;
 }
 
-void rec_solve(const bpl::db &storage,std::vector<term>& goal_list,ds::environment& env, const std::vector<std::string>& vars){
-  if(goal_list.empty()){
-    std::cerr << "found solution" << std::endl;
-    describe_solution(storage,env,vars);
-    return;
+enum solve_code { NO_SOLUTION = 0, SOLUTION_CONTINUE = 1, SOLUTION_STOP = 2 };
+
+bool prompt_stop() {
+  std::string line;
+  std::getline(std::cin, line);
+  line = parser::trim(line);
+  switch (line.front()) {
+    case '.':return true;
+    case ';':return false;
+    default:assert("Invalid command" == nullptr);
   }
-  term pr = std::move(goal_list.back()); goal_list.pop_back();
+}
+
+solve_code rec_solve(const bpl::db &storage, std::vector<term> &goal_list, ds::environment &env, const std::vector<std::string> &vars) {
+  if (goal_list.empty()) {
+    //std::cerr << "found solution" << std::endl;
+    describe_solution(storage, env, vars);
+    std::cout.flush();
+    return SOLUTION_CONTINUE;
+  }
+  term pr = std::move(goal_list.back());
+  goal_list.pop_back();
   auto ck = env.get_checkpoint();
-  for(const db::predicate::clause& cl : storage.predicates[pr.predicate_id].clauses){
+
+  solve_code any_solution = NO_SOLUTION;
+
+  for (const db::predicate::clause &cl : storage.predicates[pr.predicate_id].clauses) {
     uint32_t delta = env.nodes.size();
-    if(env.unify_with_clause(pr.args,cl)){
-      for(auto it = cl.rhs.crbegin();it!=cl.rhs.crend();++it)goal_list.emplace_back(*it, delta);
+    if (env.unify_with_clause(pr.args, cl)) {
+      for (auto it = cl.rhs.crbegin(); it != cl.rhs.crend(); ++it)goal_list.emplace_back(*it, delta);
       auto n = cl.rhs.size();
-
-      rec_solve(storage,goal_list,env,vars);
-
-      goal_list.resize(goal_list.size()-n);
+      if (any_solution && prompt_stop()) return SOLUTION_STOP;
+      solve_code ret = rec_solve(storage, goal_list, env, vars);
+      if (ret == SOLUTION_STOP)return SOLUTION_STOP;
+      goal_list.resize(goal_list.size() - n);
+      any_solution = std::max(any_solution, ret);
     }
     env.restore_checkpoint(ck);
   }
   goal_list.emplace_back(std::move(pr));
+  return any_solution;
 }
 
 void execute_query(bpl::db &storage, std::vector<parser::term> query_goal) {
@@ -100,18 +127,18 @@ void execute_query(bpl::db &storage, std::vector<parser::term> query_goal) {
   ds::environment env;
   for (const parser::term &term : query_goal)for (const parser::expression &expr : term.args)fill_vartable(vartable, expr, env);
   std::vector<std::string> vars(vartable.size());
-  for(const auto& [s,i] : vartable)vars[i]=s;
+  for (const auto&[s, i] : vartable)vars[i] = s;
   std::vector<term> goal_list;
   for (const parser::term &pterm : query_goal) {
     goal_list.emplace_back();
-    term& t = goal_list.back();
+    term & t = goal_list.back();
     assert(storage.symbol_table.find(pterm.pred) != storage.symbol_table.end());
     assert(storage.symbol_table.find(pterm.pred)->second.type == db::symbol_descr::S_PREDICATE);
     t.predicate_id = storage.symbol_table.find(pterm.pred)->second.id;
     assert(storage.predicates[t.predicate_id].degree == pterm.args.size());
     for (const parser::expression &expr : pterm.args)t.args.push_back(make_insert_expr_id(vartable, expr, storage, env));
   }
-  std::reverse(goal_list.begin(),goal_list.end());
-  rec_solve(storage,goal_list,env,vars);
+  std::reverse(goal_list.begin(), goal_list.end());
+  if (rec_solve(storage, goal_list, env, vars) == NO_SOLUTION)std::cout << "false." << std::endl;
 }
 }
